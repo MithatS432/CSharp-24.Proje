80789dc6a3cf3c155e913baade9c92a1b945208f
Mithat <mithatsirmali@gmail.com>
2025-05-23 16:27:50 +0300

    Add files via upload

---
+++ Kalıtım (Inheritance)2/Kalıtım (Inheritance)2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalıtım__Inheritance_2
+{
+    internal class Program
+    {
+        class Calisan
+        {
+            public string AdSoyad { get; set; }
+
+            public void BilgiGoster()
+            {
+                Console.WriteLine($"Çalışan: {AdSoyad}");
+            }
+        }
+
+        class Yonetici : Calisan
+        {
+            public string Departman { get; set; }
+
+            // new ile metodu gizliyoruz (shadowing)
+            public new void BilgiGoster()
+            {
+                Console.WriteLine($"Yönetici: {AdSoyad}, Departman: {Departman}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Calisan c1 = new Calisan { AdSoyad = "Ali Yılmaz" };
+            c1.BilgiGoster();
+
+            Yonetici y1 = new Yonetici { AdSoyad = "Ayşe Kaya", Departman = "İK" };
+            y1.BilgiGoster();
+
+            // new ile gölgelemenin farkı burada görülür:
+            Calisan c2 = new Yonetici { AdSoyad = "Mehmet Demir", Departman = "Satış" };
+            c2.BilgiGoster(); // Base sınıfın metodu çalışır
+        }
+    }
+}
